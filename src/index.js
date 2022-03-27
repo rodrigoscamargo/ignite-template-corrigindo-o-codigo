cef452ca7d8e68dd27ac9f0dff66b11304fd12e7
Rodrigo Santana Camargo <rodrigo.camargo@wappycode.com>
2022-03-27 20:33:54 -0300

    corrigindo o código

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { validate } = require("uuid");
 
 const { v4: uuid } = require("uuid");
 
@@ -23,20 +24,28 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
-
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+  if (!validate(id)) {
+    return response.status(404).json({ error: "Mensagem do erro" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Mensagem do erro" });
+  }
+
+  const likes = repositories[repositoryIndex].likes;
+
+  const repository = { ...repositories[repositoryIndex], ...updatedRepository, ...{ likes: likes } };
 
   repositories[repositoryIndex] = repository;
 
@@ -46,15 +55,19 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (!validate(id)) {
+    return response.status(404).json({ error: "Mensagem do erro" });
+  }
 
-  if (repositoryIndex > 0) {
-    return response.status(404).json({ error: "Repository not found" });
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Mensagem do erro" });
   }
 
   repositories.splice(repositoryIndex, 1);
 
-  return response.status(204).send();
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
@@ -63,12 +76,12 @@ app.post("/repositories/:id/like", (request, response) => {
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+    return response.status(404).json({ error: "Mensagem do erro" });
   }
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
